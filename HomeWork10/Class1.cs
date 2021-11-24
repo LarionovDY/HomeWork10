746afe9906d91296206093a2df0509f99a669727
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-11-24 22:21:15 +0300

    Добавьте файлы проекта.

---
+++ HomeWork10/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    class Angle
+    {
+        private int grad;
+        public int Grad
+        {
+            get { return grad; }
+            set
+            {
+                if (value >= 0 && value < 360)
+                {
+                    grad = value;
+                }
+                else if (value >= 360)
+                {
+                    grad = value % 360;
+                }
+            }
+        }
+        private int minute;
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+
+                if (value >= 0 && value <= 59)
+                {
+                    minute = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод, значение должно составлять от 0 до 59, присвоено значение по умолчанию (0).");
+                }
+            }
+        }
+        private int second;
+        public int Second
+        {
+            get { return second; }
+            set
+            {
+                if (value >= 0 && value <= 59)
+                {
+                    second = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод, значение должно составлять от 0 до 59, присвоено значение по умолчанию (0).");
+                }
+            }
+        }
+        public Angle()
+        {
+            grad = 0;
+            minute = 0;
+            second = 0;
+        }
+        private Angle(int grad, int minute, int second)
+        {
+            this.grad = grad;
+            this.minute = minute;
+            this.second = second;
+        }
+        public double ToRadian(int grad, int minute, int second)
+        {
+            double radian = (grad + (double)minute / 60 + (double)second / 3600) * Math.PI / 180;
+            Console.WriteLine($"Значение угла в радианах: {radian:f3}");
+            return radian;
+        }
+    }
+}
---
+++ HomeWork10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    //Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+    //Реализовать класс, в котором указанные значения представлены в виде свойств.
+    //Для свойств предусмотреть проверку корректности данных.
+    //Класс должен содержать конструктор для установки начальных значений,
+    //а также метод ToRadians для перевода угла в радианы.Создать объект на основе разработанного класса.
+    //Осуществить использование объекта в программе.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var angle = new Angle();            
+            Console.WriteLine("Введите пожалуйста значение угла в градусах, минутах, секундах");
+            Console.WriteLine("Градусы:");
+            angle.Grad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Минуты:");
+            angle.Minute = int.Parse(Console.ReadLine());
+            Console.WriteLine("Секунды:");
+            angle.Second = int.Parse(Console.ReadLine());
+            double radian = angle.ToRadian(angle.Grad, angle.Minute, angle.Second);
+            double cosAngle1 = Math.Cos(radian);
+            Console.WriteLine($"Косинус введенного угла: {Math.Cos(radian):f3}, синус: {Math.Sin(radian):f3}");            
+            Console.ReadKey();
+        }
+    }
+    
+}
